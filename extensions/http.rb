e5203dbaa344d46d9bf68700aa3aced61376a382
Mikkel Kroman <mk@uplink.io>
2014-04-29 02:41:29 +0200

    Added error handling for http extension

--- extensions/http.rb
+++ extensions/http.rb
@@ -29,17 +29,38 @@ Extension :http do
 
     # Set the request callback proc.
     def success &block
-      @client.callback &block
+      @client.callback do
+        if response_header.successful?
+          begin
+            block.call
+          rescue Exception => exception
+            p exception
+          end
+        else
+          @client.fail "expected response code 200"
+        end
+      end
     end
 
     # Set the request error proc.
     def error &block
-      @client.errback &block
+      @client.errback do
+        begin
+          block.call
+        rescue Exception => exception
+          p exception
+        end
+      end
     end
 
     # @returns The HTTP response.
     def response
       @client.response
+    end
+
+    # @returns The HTTP response header.
+    def response_header
+      @client.response_header
     end
   end
 
@@ -48,16 +69,14 @@ Extension :http do
   # @option options [optional, Symbol] :format The response format, can be :json.
   def get uri, *params, &block
     options    = params.last.is_a?(Hash) ? params.pop : {}
-    connection = EM::HttpRequest.new uri
+    connection = create_connection uri, options
+    request    = connection.get *params
 
-    case options[:format]
-    when :json
-      connection.use EM::Middleware::JSONResponse
+    HTTPContext.new(request).tap do |context|
+      context.instance_eval &block if block_given?
     end
-
-    request = connection.get *params
-
-    HTTPContext.new(request).instance_eval &block
+  rescue => exception
+    p exception
   end
 
   # Return a new http context with a newly initiated post request.
@@ -66,6 +85,18 @@ Extension :http do
   # @option options [optional, Symbol] :format The response format, can be :json.
   def post uri, *params, &block
     options    = params.last.is_a?(Hash) ? params.pop : {}
+    connection = create_connection uri, options
+    request    = connection.post *params
+    
+    HTTPContext.new(request).tap do |context|
+      context.instance_eval &block if block_given?
+    end
+
+  rescue => exception
+    p exception
+  end
+
+  def create_connection uri, options
     connection = EM::HttpRequest.new uri
 
     case options[:format]
@@ -73,8 +104,8 @@ Extension :http do
       connection.use EM::Middleware::JSONResponse
     end
 
-    request = connection.post *params
-
-    HTTPContext.new(request).instance_eval &block
+    connection
+  rescue => exception
+    p exception
   end
 end
