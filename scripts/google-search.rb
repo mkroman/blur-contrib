45157146880e6aeaa125bec5bfe78657035981fd
Mikkel Kroman <mk@uplink.io>
2014-04-29 02:43:42 +0200

    Updated google search script

--- scripts/google-search.rb
+++ scripts/google-search.rb
@@ -4,44 +4,55 @@ require 'oj'
 require 'multi_json'
 require 'htmlentities'
 
-Script :google_search, using: %w{http}, includes: [Commands] do
+Script :google_search, uses: %w{http}, includes: [Commands] do
   Author "Mikkel Kroman <mk@uplink.io>"
   Version "0.1"
-  Description "Search for stuff on google.com"
+  Description "Provides a method to search google"
 
   # Script constants.
   SearchURI = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=%s&rsz=1"
 
-  # Called when the script has been loaded.
   def loaded
+    # Initialize the HTML entities decoder
     @decoder = HTMLEntities.new
   end
 
-  # Register the .g/.google command.
   command %w{g google} do |user, channel, args|
     unless args
       return channel.say format "Usage:\x0F .g <query>"
     end
 
     search args do |title, uri|
-      channel.say format "#{@decoder.decode title}\x0F - #{uri}"
+      if title
+        channel.say format "#{@decoder.decode title}\x0F - #{uri}"
+      else
+        channel.say format "No results"
+      end
     end    
   end
   
-  # Search for something.
-  #
-  # @yields [Title, URL] or nil
   def search query
     search_uri = SearchURI % URI.escape(query)
     context = http.get search_uri, format: :json
 
     context.success do
-      results = context.response['responseData']['results']
+      begin
+        if context.response['responseStatus'] != 200
+          return yield nil
+        end
 
-      if results.any?
-        result = results.first
+        results = context.response['responseData']['results']
 
-        yield result['titleNoFormatting'], result['unescapedUrl']
+        if results.any?
+          result = results.first
+
+          yield result['titleNoFormatting'], result['unescapedUrl']
+        else
+          yield nil
+        end
+      rescue Exception => e
+        yield e.message, e.class.to_s
+        p e.backtrace
       end
     end
 
